22646ebe13de72f9fd559850704ce954c18d901a
fabrizio.filieri <fabrizio.filieri@d4d423da-bc08-11dd-ad1f-f1f9622dbc98>
2009-01-02 12:54:07 +0000

    Classi per la gestione dei messaggi su stream

--- trunk/lib/src/xdisk/net/MessageInputStream.java
+++ trunk/lib/src/xdisk/net/MessageInputStream.java
@@ -1,5 +1,65 @@
 package xdisk.net;
 
-public class MessageInputStream extends MessageInput {
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+/**
+ * Filtro di input per la ricezione di messaggi su un input stream agganciato.
+ *  
+ * @author Fabrizio Filieri
+ * @version 1.0 2/11/2009
+ *
+ */
+public class MessageInputStream extends MessageInput 
+{
+	/**
+	 * L'input stream agganciato al filtro
+	 */
+	protected InputStream inputStream;
+	
+	/**
+	 * Il filtro utilizzato per la ricezione delle meta informazioni agganciato
+	 * all'input stream collegato al filtro
+	 */
+	protected DataInputStream dataInput;
+	
+	/**
+	 * Il buffer di byte con i dati letti dall'input stream
+	 */
+	protected byte[] buffer;
+
+	/**
+	 * Crea un nuovo filtro per la ricezione di messaggi su un input stream 
+	 * collegato.
+	 * 
+	 * @param in 
+	 */
+	public MessageInputStream(InputStream in) 
+	{
+		super(null);
+		
+		inputStream = in;
+		
+		dataInput = new DataInputStream(inputStream);
+	}
+
+	@Override
+	public void receive() throws IOException 
+	{
+		synchronized (inputStream) 
+		{
+			
+			int size = dataInput.readInt();
+		
+			buffer = new byte[size];
+		
+			dataInput.readFully(buffer);
+		}
+		
+		this.in = new ByteArrayInputStream(buffer);
+	}
 
 }
--- trunk/lib/src/xdisk/net/MessageOutput.java
+++ trunk/lib/src/xdisk/net/MessageOutput.java
@@ -1,5 +1,34 @@
 package xdisk.net;
 
-public abstract class MessageOutput {
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Filtro stream base astratto per la gestione di messaggi sugli stream.
+ * Estende il filtro {@link DataOutputStream} per aggiungere funzionalità
+ * di creazione dei messaggi.
+ * 
+ * @author Fabrizio Filieri
+ * @version 1.0 2/1/2009
+ */
+public abstract class MessageOutput extends DataOutputStream
+{
+
+	/**
+	 * Crea un nuovo {@link MessageOutput} attaccato allo stream out
+	 * @param out Lo stream a cui è attaccato il filtro
+	 */
+	public MessageOutput(OutputStream out) 
+	{
+		super(out);
+	}
+	
+	/**
+	 * Invia il contenuto del messaggio presente nel buffer.
+	 * 
+	 * @throws IOException
+	 */
+	public abstract void send() throws IOException;
 
 }
